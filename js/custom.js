fd74e9f7b63aef0dc1088a2457582c0768927106
AndreeaPenu <andreea_penu@hotmail.Com>
2018-01-19 19:41:37 +0100

    frontend add

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,36 @@ $(function(){
         $("#big-image img").eq(index).show().siblings().hide();
     });
 });
+
+
+function goLogin() {
+    var x = document.getElementById("nav1-welcome");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+$(document).ready(function() {
+    $('#user-form').validate({
+   rules: {
+     email: {
+       required: true,
+       email: true
+     },
+   },
+   
+   highlight: function(element, errorClass, validClass) { 
+     $(element).nextAll('.form-control-feedback').show().removeClass('glyphicon-ok').addClass('glyphicon-remove');
+     $(element).addClass(errorClass).removeClass(validClass);
+     $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+   },
+   success: function(element) {
+     $(element).nextAll('.form-control-feedback').show().removeClass('glyphicon-remove').addClass('glyphicon-ok');
+  element.closest('.form-group').removeClass('has-error').addClass('has-success');
+     $(element).remove();
+   }
+ });
+});
