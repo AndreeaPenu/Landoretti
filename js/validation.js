c1a2f8b9b4b13ad65c06c671a9cacf4542c58654
AndreeaPenu <andreea_penu@hotmail.Com>
2018-08-01 16:57:39 +0200

    checkmark success validation

--- js/validation.js
+++ js/validation.js
@@ -11,8 +11,11 @@ $(function(){
         errorPlacement: function (error, element) {
             var name = $(element).attr("name");
             $('.error-message').show();
-           $('.error-message').append( " <li> Please enter a " + name + " for your auction </li>" );
+            $('.error-message').append( " <li> Please enter a " + name + " for your auction </li>" );
         },
+        success: function(element) {
+            $('#success').css('display','block');
+          }
     });
 });
 
