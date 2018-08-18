0609c2303f27431211c1198633e4b139393c5dca
AndreeaPenu <andreea_penu@hotmail.Com>
2018-08-18 16:51:42 +0200

    edit hover overview page

--- js/timer.js
+++ js/timer.js
@@ -6,19 +6,19 @@ var x = setInterval(function() {
 
     // Get todays date and time
     var now = new Date().getTime();
-    
+
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
-    
+
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (7000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    
+
     // Output the result in an element with id="demo"
-    document.getElementById("time").innerHTML = days + "d " + hours + "h "
-    + minutes + "m ";
-    
+    document.getElementById("time").innerHTML = days + "d " + hours + "u " +
+        minutes + "m ";
+
     // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
