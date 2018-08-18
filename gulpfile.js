1ab2a2fc1ce26b4f41e1ba7c4a43718ff53dcde7
AndreeaPenu <andreea_penu@hotmail.Com>
2018-08-18 13:09:42 +0200

    performance edits

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
+const imageminMozjpeg = require('imagemin-mozjpeg');
 
 var elixir = require('laravel-elixir');
 
@@ -12,27 +13,31 @@ elixir(function(mix) {
 // optimize images
 gulp.task('imageMin', () =>
     gulp.src('img/*')
-    .pipe(imagemin())
+    .pipe(imagemin([
+        imageminMozjpeg({
+            quality: 50
+        })
+    ]))
     .pipe(gulp.dest('dist/images'))
 );
 
 // minify JS
-gulp.task('minify', function(){
+gulp.task('minify', function() {
     gulp.src('js/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
 });
 
 // compile sass
-gulp.task('sass', function(){
+gulp.task('sass', function() {
     gulp.src('scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('css'));
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css'));
 });
 
 gulp.task('default', ['imageMin', 'minify', 'sass']);
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
     gulp.watch('js/*.js', ['minify']);
     gulp.watch('img/*', ['imageMin']);
     gulp.watch('scss/*.scss', ['sass']);
